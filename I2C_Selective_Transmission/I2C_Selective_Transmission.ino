f2bfb838c0a53b63ba25fadb7c2972faff551bee
Rohan K <84638053+vaibruce@users.noreply.github.com>
2024-11-07 12:03:08 +0530

    Update I2C_Selective_Transmission.ino

--- I2C_Selective_Transmission/I2C_Selective_Transmission.ino
+++ I2C_Selective_Transmission/I2C_Selective_Transmission.ino
@@ -1,11 +1,11 @@
 #include <Wire.h>
 String message1="";
 String message2="";
-int slaveAddress[]={5,8}; //Enter the slave addresses of other arduinos
-int numSlaves = sizeof(slaveAddress) / sizeof(slaveAddress[0]);
+int address;
+char first;
 
 void setup(){
-  Wire.begin(2);
+  Wire.begin(8);
   Wire.onReceive(recieve);
   Serial.begin(9600);
   Serial.println("Enter the message to be sent with the first character ");
@@ -13,30 +13,32 @@ void setup(){
   Serial.println("For Example:5Hi, sends Hi to the arduino at address 5");
 }
 
-
 void loop(){
   message1="";
   if(Serial.available()>0)
   {
-    message2=Serial.readStringUntil('\n');
-    for(int i=0;i<numSlaves;i++){
-    Wire.beginTransmission(slaveAddress[i]);
+   message2=Serial.readStringUntil('\n');
+    first=message2[0];
+    address=(int)first-48;
+    message2.remove(0,1);
+    Serial.println(" "+message2+" sent to address: "+ String(address));
+    Wire.beginTransmission(address);
     Wire.write(message2.c_str());
     Wire.endTransmission();
-    }
+   
     
-  }  
- 
+  }
+     
   delay(100);
 
 }
 
 void recieve()
-{ message1="";
+{ message2="";
   while(Wire.available()>0)
   {
     char c=Wire.read();
-    message1+=c;
+    message2+=c;
   }
-   Serial.println(message1);
+   Serial.println(message2);
   }
